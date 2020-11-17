cbf127a9964bd429b552e64496959adc6e3014e3
LeonardoAlves <46458626+leonardoAlves99@users.noreply.github.com>
2020-11-17 20:25:01 -0300

    Add files via upload

---
+++ Trabalho Prog/Javascript.js
@@ -0,0 +1,17 @@
+(function (){
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD60ZTLw9GqddHZxloaHzDxPnABBxTW8ps",
+    authDomain: "projsd-31dae.firebaseapp.com",
+    databaseURL: "https://projsd-31dae.firebaseio.com",
+    projectId: "projsd-31dae",
+    storageBucket: "projsd-31dae.appspot.com",
+    messagingSenderId: "4141505031",
+    appId: "1:4141505031:web:8baaa0e7b040365f6968a2",
+    measurementId: "G-FZK9FQQP0J"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+}
